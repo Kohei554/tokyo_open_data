ed1d8c7e6e26d2260b5156367f3c631fea49614e
chanon-mike <85671768+chanon-lim@users.noreply.github.com>
2021-09-09 22:28:49 +0700

    Update styling and requirements.txt
    Update python and HTML code
    Add CSS file
    Add requirements.txt file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 import requests
 import os
 
@@ -6,6 +7,7 @@ import os
 app = Flask(__name__)
 API_KEY = os.getenv("API_KEY")
 ENDPOINT = "https://api-tokyochallenge.odpt.org/api/v4/"
+Bootstrap(app)
 
 # Request the data from API
 data_type = "odpt:TrainInformation?"
@@ -18,11 +20,15 @@ data = response.json()
 
 @app.route("/")
 def home():
-    time = [information['dc:date'].split('T')[1].split('+')[0] for information in data]
+    railways = [information['owl:sameAs'].split(':')[1].split(".") for information in data]
+    # operators = [railway[0] for railway in railways]
+    railways = [" ".join(railway) if len(railway) != 1 else railway[0] for railway in railways]
     train_status = [information['odpt:trainInformationText']['ja'] for information in data]
-    railways = [information['owl:sameAs'].split(':')[1] for information in data]
-    return render_template("index.html", time=time, train_status=train_status, railways=railways)
+    time = [":".join(information['dc:date'].split('T')[1].split('+')[0].split(":")[:2]) for information in data]
+    return render_template("index.html", railways=railways, train_status=train_status, time=time)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
