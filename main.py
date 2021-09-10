b50346a4dc87f3fe1286312d7d978199cb7747af
chanon-mike <85671768+chanon-lim@users.noreply.github.com>
2021-09-10 23:18:24 +0700

    Update main.py and index.html
    Change the data from list to dict
    Add each railways logo
    Update styles.css

--- main.py
+++ main.py
@@ -21,12 +21,24 @@ data = response.json()
 @app.route("/")
 def home():
     railways = [information['owl:sameAs'].split(':')[1].split(".") for information in data]
-    # operators = [railway[0] for railway in railways]
-    railways = [" ".join(railway) if len(railway) != 1 else railway[0] for railway in railways]
-    train_status = [information['odpt:trainInformationText']['ja'] for information in data]
-    time = [":".join(information['dc:date'].split('T')[1].split('+')[0].split(":")[:2]) for information in data]
-    return render_template("index.html", railways=railways, train_status=train_status, time=time)
+    operators = [railway[0] for railway in railways]
+    train_dict = {}
 
+    # Loop through operators and railways to create dictionary of information
+    for i in range(len(operators)):
+        train_status = [information['odpt:trainInformationText']['ja'] for information in data if information['owl:sameAs'].split(':')[1].split(".")[0] == operators[i]]
+        time = [":".join(information['dc:date'].split('T')[1].split('+')[0].split(":")[:2]) for information in data if information['owl:sameAs'].split(':')[1].split(".")[0] == operators[i]]
+        train_dict.update(
+            {
+                operators[i]: {
+                    "railways": [railway[-1] for railway in railways if railway[0] == operators[i]],
+                    "train_status": train_status,
+                    "time": time
+                }
+            }
+        )
+
+    return render_template("index.html", train_dict=train_dict)
 
 if __name__ == "__main__":
     app.run(debug=True)
